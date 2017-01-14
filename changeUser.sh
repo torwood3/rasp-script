1c2dcb9f48f6c87072c141b7cc1f7d6c502c41ae
torwood3 <torwood@hotmail.fr>
2017-01-14 22:15:05 +0100

    update change user script

--- changeUser.sh
+++ changeUser.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 echo "Setup for raspberry"
 
 echo "User"
 read -p "What name for the new pi?" newuser
-adduser $newuser
+sudo adduser $newuser
 
 for i in `groups pi`
 do
     if [ "$i" != ":" ]; then
-		adduser $newuser $i
+		sudo adduser $newuser $i
     fi
 done
 
 echo "Suppression of the pi user"
-deluser pi
+sudo deluser pi
+
+echo "Change root password"
+sudo passwd root
 
 echo "Block ssh for root"
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 
-echo "Reboot of SSH"
-/etc/init.d/sshd restart
+sudo /etc/init.d/sshd restart
+
+
+
+
+
+
+
+
+
+
+
 
